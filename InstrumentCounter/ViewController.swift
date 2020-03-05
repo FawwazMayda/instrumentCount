be203b9a12172d526b227a024ccb9d995b0b1931
fawwazmayda <maydafawwaz@gmail.com>
2020-03-05 16:27:26 +0700

    Give a label

--- InstrumentCounter/ViewController.swift
+++ InstrumentCounter/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var tromboneLabel: UILabel!
+    
+    @IBOutlet weak var guitarLabel: UILabel!
+    
+    @IBOutlet weak var drumLabel: UILabel!
+    @IBOutlet weak var pianoLabel: UILabel!
+    
+    var tromboneCount = 0
+    var guitarCount = 0
+    var drumCount = 0
+    var pianoCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func tromboneTapped(_ sender: UIButton) {
+        if(tromboneCount < 6){
+            tromboneCount += 1
+            tromboneLabel.text = "\(tromboneCount) tap"
+        } else {
+            let alert = UIAlertController(title: "Message", message: "Your instrument reach the max value", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
